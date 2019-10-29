d59f2d9a83241ce3283b5e69071f15b151c86617
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-10-29 10:19:19 -0600

    last minute style

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = props => {
     return (
         <div 
-        style = {props.todo.completed ? {textDecoration: 'line-through'} : null }
+        style = {props.todo.completed ? {textDecoration: 'line-through', color: 'red', transition: '0.25s'} : null }
         onClick = {() => props.handleToggleComplete(props.todo.id)}
         >
         {props.todo.task}
